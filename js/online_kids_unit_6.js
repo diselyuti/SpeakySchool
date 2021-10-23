7daca5f87f0f6308590da65a0ab9d37a28d7b5f3
Dima <dima.selyutin.03@gmail.com>
2021-10-23 19:18:52 +0300

    offline_kids page done
    the choice of an offline lesson has been implemented

--- js/online_kids_unit_6.js
+++ js/online_kids_unit_6.js
@@ -11,6 +11,16 @@ $( document ).ready(function() {
         setIndivMin60();
         setTypeAndTime(typeState, timeState);
     });
+    $(".indivOffline").click(function (e) { 
+        e.preventDefault();
+        typeState = "indiv";
+        $(".min30").css("display", "flex");
+        $(".middle").css("display", "none");
+        $(".tablet").css("margin", "0 40px 0 40px");
+        $(".someInfo").css("display", "none");
+        setIndivOffline();
+        setTypeAndTime(typeState, timeState);
+    });
     $(".common").click(function (e) { 
         e.preventDefault();
         typeState = "common";
@@ -19,6 +29,26 @@ $( document ).ready(function() {
         $(".middle").css("display", "none");
         $(".tablet").css("margin", "0 40px 0 40px");
         setCommonMin60();
+        setTypeAndTime(typeState, timeState);
+    });
+    $(".commonOffline").click(function (e) { 
+        e.preventDefault();
+        typeState = "common";
+        timeState = "min60";
+        $(".min30").css("display", "none");
+        $(".middle").css("display", "none");
+        $(".tablet").css("margin", "0 40px 0 40px");
+        $(".someInfo").css("display", "none");
+        setCommonOffline();
+        setTypeAndTime(typeState, timeState);
+    });
+    $(".groups").click(function (e) { 
+        e.preventDefault();
+        typeState = "groups";
+        $(".middle").css("display", "flex");
+        $(".tablet").css("margin", "0 40px 0 40px");
+        $(".someInfo").css("display", "block");
+        setGroupsOffline();
         setTypeAndTime(typeState, timeState);
     });
     $(".min30").click(function (e) { 
@@ -45,10 +75,17 @@ function setTypeAndTime(typeState, timeState){
         case "common":
             $("#lineCommon").css("background-color", "#236967");
             $("#lineIndiv").css("background-color", "#94C6C4");
+            $("#lineGroups").css("background-color", "#94C6C4");
+            break;
+        case "groups":
+            $("#lineGroups").css("background-color", "#236967");
+            $("#lineIndiv").css("background-color", "#94C6C4");
+            $("#lineCommon").css("background-color", "#94C6C4");
             break;
         default:
             $("#lineCommon").css("background-color", "#94C6C4");
             $("#lineIndiv").css("background-color", "#236967");
+            $("#lineGroups").css("background-color", "#94C6C4");
             break;
     }
     switch (timeState) {
@@ -62,6 +99,7 @@ function setTypeAndTime(typeState, timeState){
             break;
     }
 }
+// online
 function setIndivMin60(){
     $("#leftH2").text("4 занятия по 60 мин");
     $("#middleH2").text("8 занятий по 60 мин");
@@ -92,4 +130,33 @@ function setCommonMin60(){
     $("#priceRight").text("16200 ₽");
     $("#leftImg").attr("src", "../img/images/20067.svg");
     $("#rightImg").attr("src", "../img/images/20068.svg");
+}
+
+// offline
+function setIndivOffline(){
+    $("#leftH2").text("4 занятия по 60 мин");
+    $("#rightH2").text("12 занятия по 60 мин");
+    $("#priceLeft").text("8800 ₽");
+    $("#priceRight").text("17600 ₽");
+    $("#leftImg").attr("src", "../img/images/20067.svg");
+    $("#rightImg").attr("src", "../img/images/20068.svg");
+}
+function setCommonOffline(){
+    $("#leftH2").text("4 занятия по 60 мин");
+    $("#rightH2").text("8 занятий по 60 мин");
+    $("#priceLeft").text("3600 ₽");
+    $("#priceRight").text("6800 ₽");
+    $("#leftImg").attr("src", "../img/images/20067.svg");
+    $("#rightImg").attr("src", "../img/images/20068.svg");
+}
+function setGroupsOffline(){
+    $("#leftH2").text("4 занятия по 45-60 мин*");
+    $("#middleH2").text("8 занятий по 45-60 мин*");
+    $("#rightH2").text("12 занятий по 45-60 мин*");
+    $("#priceLeft").text("4300 ₽");
+    $("#priceMiddle").text("7900 ₽");
+    $("#priceRight").text("11850 ₽");
+    $("#leftImg").attr("src", "../img/images/20061.svg");
+    $("#middleImg").attr("src", "../img/images/20062.svg");
+    $("#rightImg").attr("src", "../img/images/20063.svg");
 }
